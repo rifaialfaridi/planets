766ee3dff4ec4b3bceda5e9927e455daced66969
rifaialfaridi <rifaialfaridi0734@gmail.com>
2022-06-14 12:52:30 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const latar = document.querySelector('.matahari');
+const bg = document.querySelector('body');
+
+latar.addEventListener('click', function () {
+  document.body.classList.toggle('latar');
+});
